92e5e978076451fb6b3a67ba337b3b52a5949093
yo-its-anas <anasejaz4042@gmail.com>
2026-03-16 12:45:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ r = redis.Redis(host=redis_host, port=redis_port)
 @app.route("/")
 def hello():
     count = r.incr("visits")
-    return f"Hello from Zynex Solutions! Jenkins CI/CD (Pipeline Completed!!!) Visits: {count}"
+    return f"Hello from Zynex Solutions! Jenkins CI/CD with Dockerhub !! Visits: {count}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
